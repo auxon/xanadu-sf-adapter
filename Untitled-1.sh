97f88c2c0fd7b5be77ef2496179197309ab5d4aa
Richard Anthony Hein <richard.hein@gmail.com>
2024-04-03 11:42:06 +0000

    remove bitcoin build stuff

--- Untitled-1.sh
+++
@@ -1,4 +0,0 @@
-docker run -d --rm --name bitcoind -v /teamspace/studios/bitcoin-dataset/.bitcoin:/data bitcoinsv/bitcoin-sv
-docker network create bitcoin
-docker run -d --rm --name bitcoind -v /teamspace/studios/bitcoin-dataset/.bitcoin:/data --network bitcoin bitcoinsv/bitcoin-sv
-docker run --rm --network bitcoin bitcoinsv/bitcoin-sv bitcoin-cli -rpcconnect=bitcoind getinfo
